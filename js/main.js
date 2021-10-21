4b940be904f18edf4a9b884d61cb3f67e7fdf053
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-21 14:47:51 +0200

    add sketched task

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('test')
+// TASK CALC TRAIN TICKET PRICE
+
+// 1. ask user km to do
+//    -declaration const_ask value (convert value)
+
+// 2. ask user age
+//    -declaration const_ask value (convert value)
+
+// 3. calculate standard price
+//    -declaration const_km * 0.21€
+
+// 4. declaration let final price
+// 5. calculate price for < 18
+//    -condition age < 18 --> final price=(standard price - ((standard price * 20) / 100))
+
+// 6. calculate price for >= 65
+//    -condition age > 65 --> final price=(standard price - ((standard price * 40) / 100))
+
+// 7. calculate price for  18 < age < 65
+//    -final price = standard price
+
+// 8. video print result 
+//    -id selection_innerHtml final price
